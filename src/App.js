f5595fe61a299fabe7d6d26c5719548c134cf625
acfernanda <acfernanda98@gmail.com>
2022-05-23 11:32:50 -0300

    Commit project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [file, setFile] = useState();
+  console.log(file);
+
+  const send = (e) => {
+    const data = new FormData();
+    data.append("file", file);
+
+    axios
+      .post("http://localhost:3000/upload", data)
+      .then((res) => {
+        console.log(res);
+        alert("Enviado");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>POC - Buffer e Upload de arquivos com Multer</p>
+        <form action="#">
+          <input
+            type="file"
+            id="file"
+            onChange={(e) => {
+              const file = e.target.files[0];
+              setFile(file);
+            }}
+            required
+          />
+        </form>
+        <button onClick={send}>Enviar</button>
       </header>
     </div>
   );
